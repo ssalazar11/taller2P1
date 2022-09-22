dbd1ad2f315217c3a3377b320104ef14ffe610b5
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-09-21 20:21:47 -0500

    montado de taller 2

---
+++ mlappproject/petclassifier/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from platform import architecture
+# Create your models here.
+
+class mlModels(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.CharField(max_length=250)
+    architecture=models.FileField(upload_to='mlmodels/')
+    weights=models.FileField(upload_to='mlmodels/')
+    priority=models.PositiveSmallIntegerField(null=True)
